86790025f05e19d7053eacc014bf3474b3171b06
Ryan Delaney <ryan.del17@gmail.com>
2015-08-13 19:00:12 -0500

    Implemented Particle elastic collision equations with the help of Sedgewick.

--- src/com/ryand/Particle.java
+++ src/com/ryand/Particle.java
@@ -7,41 +7,70 @@ import java.util.Random;
  */
 public class Particle {
 
-    private double _pX;
-    private double _pY;
-    private double _vX;
-    private double _vY;
-    private double _radius;
-    private double _mass;
+    private double pX;
+    private double pY;
+    private double vX;
+    private double vY;
+    private double radius;
+    private double mass;
+
+    private int count;
 
     public Particle(){
         Random generator = new Random(1);
 
-        _pX = generator.nextDouble(); _pY = generator.nextDouble();
-        _vX = generator.nextDouble(); _vY = generator.nextDouble();
-        _radius = generator.nextDouble(); _mass = generator.nextDouble();
+        pX = generator.nextDouble(); pY = generator.nextDouble();
+        vX = generator.nextDouble(); vY = generator.nextDouble();
+        radius = generator.nextDouble(); mass = generator.nextDouble();
     }
     public Particle(double  pX, double pY, double vX, double vY, double radius, double mass){
-        _pX = pX; _pY = pY;
-        _vX = vX; _vY = vY;
-        _radius = radius; _mass = mass;
+        this.pX = pX; this.pY = pY;
+        this.vX = vX; this.vY = vY;
+        this.radius = radius; this.mass = mass;
     }
 
     public void move(double dt){
-        _pX = _pX + _vX*dt;
-        _pY = _pY + _vY*dt;
+        pX = pX + vX *dt;
+        pY = pY + vY *dt;
     }
 
     public int count(){
-        return 0;
+        return count;
     }
 
     public void bounceOff(Particle b){
+        double dx = b.pX - pX;
+        double dy = b.pY - pY;
+        double dvx = b.vX - vX;
+        double dvy = b.vY - vY;
 
+        double velocityDotPosition = dx*dvx + dy*dvy;
+        double distance = radius + b.radius;
+
+        //compute the normal force
+        double F = 2* mass * b.mass * velocityDotPosition / ((mass + b.mass) * distance);
+        double fx = F * dx / distance;
+        double fy = F * dy / distance;
+
+        vX += fx / mass;
+        vY += fy / mass;
+        b.vY -= fy / mass;
+        b.vY -= fy / mass;
+
+        count++;
+        b.count++;
     }
 
-    public void bounceOffHorizontalWall(){ _vY = -_vY; }
-    public void bounceOffVerticalWall(){ _vX = -_vX; }
+    public void bounceOffHorizontalWall()
+    {
+        vY = -vY;
+        count++;
+    }
+    public void bounceOffVerticalWall()
+    {
+        vX = -vX;
+        count++;
+    }
 
 
 }
